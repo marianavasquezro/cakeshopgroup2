cb5472d8e9f60bc057377f9806c56307e73d2a8e
Mariana Vasquez Rosado <152337785+marianavasquezro@users.noreply.github.com>
2024-07-08 21:59:43 -0600

    Update scripts.js
    Will use netlify for the forms

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,3 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    const form = event.target;
-    const formData = new FormData(form);
-
-    fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData).toString()
-    })
-    .then(response => {
-        if (response.ok) {
-            alert('Message sent successfully!');
-            form.reset(); // Optional: Reset form after successful submission
-        } else {
-            throw new Error('Failed to send message');
-        }
-    })
-    .catch(error => alert('Error sending message: ' + error.message));
-});
 
 
